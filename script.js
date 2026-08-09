06ef6794c39604f73adef62a081cfa91e0fcd003
ShivakumarDornala <shivakumardornla000@gmail.com>
2026-08-09 15:44:17 +0530

    d

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
 // HAMBURGER MENU
-const dropdown = document.querySelector(".dropdown-menu");
-function onToggleMenu(e) {
-  e.name = e.name == "menu" ? "close" : "menu";
-  dropdown.classList.toggle("top-16");
-}

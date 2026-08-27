de0469b76d946b719970b21341265438df4b53bc
ShivakumarDornala <shivakumardornla000@gmail.com>
2026-08-28 00:31:15 +0530

    fix humburger

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// HAMBURGER MENU
+function onToggleMenu(menuIcon) {
+  const dropdownMenu = document.querySelector(".dropdown-menu");
+
+  if (!dropdownMenu) {
+    return;
+  }
+
+  dropdownMenu.classList.toggle("-top-full");
+  dropdownMenu.classList.toggle("top-16");
+  menuIcon.setAttribute(
+    "name",
+    dropdownMenu.classList.contains("-top-full") ? "menu" : "close",
+  );
+}

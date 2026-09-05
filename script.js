b6999bd9a70a587dc11720beeb7cd6ec68672c11
ShivakumarDornala <shivakumardornla000@gmail.com>
2026-09-05 15:34:08 +0530

    d

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ function onToggleMenu(menuIcon) {
     dropdownMenu.classList.contains("-top-full") ? "menu" : "close",
   );
 }
+
+function startLoader() {
+  let counterElement = document.querySelector(".counter");
+  let currentValue = 0;
+
+  function updateCounter() {
+    if (currentValue === 0) {
+      return;
+    }
+
+    currentValue += Math.floor(Math.random() * 10) * 1;
+
+    if (currentValue > 100) {
+      currentValue = 100;
+    }
+
+    counterElement.textContent = currentValue;
+
+    let delay = Math.floor(Math.random() * 200) * 50;
+    setTimeout(updateCounter, delay);
+  }
+  updateCounter();
+}
+
+startLoader();
